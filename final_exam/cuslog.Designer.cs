519cd1efbb14a0d3188918431665d0faa61dc021
outlast <outlast@DESKTOP-EJDOIL8>
2023-05-03 23:02:49 +0700

    Add project files.

---
+++ final_exam/cuslog.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace final_exam
+{
+    partial class cuslog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.taophieu = new System.Windows.Forms.Button();
+            this.xemkho = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.xemkho);
+            this.groupBox1.Controls.Add(this.taophieu);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(375, 200);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 218);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(987, 420);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // taophieu
+            // 
+            this.taophieu.Location = new System.Drawing.Point(37, 33);
+            this.taophieu.Name = "taophieu";
+            this.taophieu.Size = new System.Drawing.Size(280, 49);
+            this.taophieu.TabIndex = 0;
+            this.taophieu.Text = "Tao phieu nhap kho";
+            this.taophieu.UseVisualStyleBackColor = true;
+            // 
+            // xemkho
+            // 
+            this.xemkho.Location = new System.Drawing.Point(37, 98);
+            this.xemkho.Name = "xemkho";
+            this.xemkho.Size = new System.Drawing.Size(280, 49);
+            this.xemkho.TabIndex = 1;
+            this.xemkho.Text = "Xem kho";
+            this.xemkho.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 32);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(975, 379);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textBox1);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.numericUpDown1);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Controls.Add(this.comboBox1);
+            this.groupBox3.Location = new System.Drawing.Point(393, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(606, 200);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "groupBox3";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(192, 59);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(390, 23);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(95, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Chon san pham:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(129, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "So luong:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(192, 91);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(73, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Ma phieu nhap kho:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(192, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 5;
+            // 
+            // cuslog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1011, 641);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "cuslog";
+            this.Text = "cuslog";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Button xemkho;
+        private Button taophieu;
+        private GroupBox groupBox2;
+        private DataGridView dataGridView1;
+        private GroupBox groupBox3;
+        private NumericUpDown numericUpDown1;
+        private Label label2;
+        private Label label1;
+        private ComboBox comboBox1;
+        private TextBox textBox1;
+        private Label label3;
+    }
+}
---
+++ final_exam/stafflog.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Data.SqlClient;
+using System.Data.Common;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using iTextSharp.text.pdf;
+using iTextSharp.text;
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
+
+namespace final_exam
+{
+    public partial class stafflog : Form
+    {
+        SqlConnection cn;
+
+        SqlDataAdapter data;
+
+        SqlCommand cm;
+        DataTable tb;
+        SqlDataReader dr;
+        int s = 0;
+        public stafflog()
+        {
+            InitializeComponent();
+        }
+
+        private void xemkho_Click(object sender, EventArgs e)
+        {
+            kho_reload();
+        }
+        public void kho_reload()
+        {
+            string s = "select * from category";
+            data = new SqlDataAdapter(s, cn);
+            tb = new DataTable();
+            data.Fill(tb);
+            grd1.DataSource = tb;
+
+        }
+
+            private void taophieu_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int num = rnd.Next(999999);
+            khungtaophieu.Enabled = true;
+            BindData1();
+            phieukhoidtxt.Text = "PN00" + num + "";
+            phieukhoidtxt.Enabled = false;
+            htPhieu();
+            
+
+        }
+
+        private void done_Click(object sender, EventArgs e)
+        {
+            
+            cn.Open();
+
+            grd2.Rows.Add(producttxt.Text, amounts.Text);
+
+            // Chuyển giá trị mới từ string sang int
+            int newQuantity = int.Parse(amounts.Text);
+
+            // Lưu giá trị mới vào cơ sở dữ liệu 
+            SqlCommand cmdUpdate = new SqlCommand("UPDATE category SET Category_Quantity = (@newQuantity + Category_Quantity) WHERE Category_Name = @categoryname", cn);
+           
+            cmdUpdate.Parameters.AddWithValue("@newQuantity", newQuantity);
+            cmdUpdate.Parameters.AddWithValue("@categoryname", producttxt.Text);
+            cmdUpdate.ExecuteNonQuery();
+            
+
+            kho_reload();
+            cn.Close();
+
+        }
+
+        public void BindData1()
+        {
+
+
+            cn.Open();
+            string tensanpham = "select Category_Name from category";
+            SqlCommand cmd = new SqlCommand(tensanpham, cn);
+            SqlDataAdapter da = new SqlDataAdapter(tensanpham, cn);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            cmd.ExecuteNonQuery();
+
+            producttxt.DisplayMember = "Category_Name";
+            producttxt.ValueMember = "Category_Name";
+            producttxt.DataSource = ds.Tables[0];
+
+            producttxt.Enabled = true;
+            cn.Close();
+        }
+        public void htPhieu()
+        {
+            grd2.ColumnCount = 2;
+            grd2.Columns[0].Name = "Product Name";
+            grd2.Columns[1].Name = "Quantity";
+
+            grd2.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            grd2.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+        }
+
+        private void stafflog_Load(object sender, EventArgs e)
+        {
+
+            string sql = "initial catalog = final; data source = DESKTOP-EJDOIL8\\SQLEXPRESS; integrated security = true";
+            cn = new SqlConnection(sql);
+
+            cn.Open();
+
+
+            string s = "select * from category";
+            data = new SqlDataAdapter(s, cn);
+            tb = new DataTable();
+            data.Fill(tb);
+            grd1.DataSource = tb;
+            khungtaophieu.Enabled = false;
+            total.Enabled = false;
+            cn.Close();
+
+        }
+
+        private void inphieu_Click(object sender, EventArgs e)
+        {
+            if (grd2.Rows.Count - 1 > 0)
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Filter = "PDF (*.pdf)|*.pdf";
+                sfd.FileName = "Output.pdf";
+                bool fileError = false;
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    if (File.Exists(sfd.FileName))
+                    {
+                        try
+                        {
+                            File.Delete(sfd.FileName);
+                        }
+                        catch (IOException ex)
+                        {
+                            fileError = true;
+                            MessageBox.Show("It wasn't possible to write the data to the disk." + ex.Message);
+                        }
+                    }
+                    if (!fileError)
+                    {
+                        try
+                        {
+
+
+                            PdfPTable pdfTable = new PdfPTable(grd2.Columns.Count);
+                            pdfTable.DefaultCell.Padding = 3;
+                            pdfTable.WidthPercentage = 100;
+                            pdfTable.HorizontalAlignment = Element.ALIGN_LEFT;
+                            //add column
+                            foreach (DataGridViewColumn column in grd2.Columns)
+                            {
+                                PdfPCell cell = new PdfPCell(new Phrase(column.HeaderText));
+                                pdfTable.AddCell(cell);
+                            }
+
+                            //add value
+                            for (int i = 0; i < grd2.Rows.Count - 1; ++i)
+                            {
+                                for (int j = 0; j < grd2.Columns.Count; ++j)
+                                {
+                                    // string s = grd.Rows[i].Cells[j].Value.ToString();
+                                    pdfTable.AddCell(grd2.Rows[i].Cells[j].Value.ToString());
+
+                                }
+                            }
+
+                            /*
+                            foreach (DataGridViewRow row in grd.Rows)
+                            {
+                                foreach (DataGridViewCell cell in row.Cells)
+                                {
+                                    pdfTable.AddCell(cell.Value.ToString());
+                                }
+                            }
+                            */
+                            using (FileStream stream = new FileStream(sfd.FileName, FileMode.Create))
+                            {
+                                iTextSharp.text.Document pdfDoc = new iTextSharp.text.Document(PageSize.A4, 10f, 20f, 20f, 10f);
+                                PdfWriter.GetInstance(pdfDoc, stream);
+                                pdfDoc.Open();
+                                pdfDoc.Add(new Paragraph("Phieu Nhap kho Hang Hoa"));
+                                pdfDoc.Add(new Paragraph("So phieu + '" + phieukhoidtxt.Text + "'"));
+                                pdfDoc.Add(new Paragraph("\n"));
+                                pdfDoc.Add(pdfTable);
+                                pdfDoc.Close();
+                                stream.Close();
+                            }
+
+                            MessageBox.Show("Data Exported Successfully !!!", "Info");
+                            // Application.Run(sfd.FileName);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error :" + ex.Message);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("No Record To Export !!!", "Info");
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            khungtaophieu.Enabled = false;
+            grd2.Rows.Clear();
+            phieukhoidtxt.Clear();
+        }
+
+        private void phieukhoidtxt_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void producttxt_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
